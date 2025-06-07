4646ea7befdd4b5750c74ec23be0d17794fb34d4
regiana60 <2472060@maranatha.ac.id>
2025-06-07 23:21:18 +0700

    pisah

--- js/contactus.js
+++ js/contactus.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
-    function check() {
-        const data = document.
-    }
-})
+function mail() {
+    var contact = {
+        name : document.getElementById("nama").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subjek").value,
+        message : document.getElementById("pesan").value
+    };
+
+    emailjs.send("service_2l7af29", "template_tlh50cx", contact).then(function(){
+        alert("Email terkirim.");
+    });
+};
+
